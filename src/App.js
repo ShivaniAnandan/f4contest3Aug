775e6cfb64bb7f0849604e7698994cbb8ec9c24e
Shivani Anandan <shivanianandhan28@gmail.com>
2023-08-26 22:06:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "../src/Style.css";
+import NavBar from "./Components/NavBar";
+import MoreBooks from "./Components/MoreBooks";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import BookBanner from "./Components/BookBanner";
 
-function App() {
+const App = () => {
+  const [bookLists, setBookLists] = useState([]);
+  const [previewlist, setPreviewList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://www.googleapis.com/books/v1/volumes?q=harry+potter&key=AIzaSyBLKk4qpY1tMkGM7IXSuszRvqf5BnFJ6dY"
+      )
+      .then((request) => {
+        updateLists(request.data.items);
+        //setPreviewList(request.data.items.slice(0, 3));
+        //console.log(previewlist);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(err);
+      });
+
+    axios
+      .get(
+        "https://www.googleapis.com/books/v1/volumes?q=Sherlock+Holmes&key=AIzaSyBLKk4qpY1tMkGM7IXSuszRvqf5BnFJ6dY"
+      )
+      .then((request) => {
+        updateLists(request.data.items);
+        //setPreviewList(request.data.items.slice(0, 3));
+        //console.log(previewlist);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(err);
+      });
+  }, []);
+
+  function updateLists(list) {
+    setBookLists((prevItem) => prevItem.concat(list));
+    setPreviewList(list.slice(0, 3));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar setBookLists={setBookLists} setPreviewList={setPreviewList} />
+      <BookBanner previewlist={previewlist} />
+      <MoreBooks bookLists={bookLists} className="gallery" />
     </div>
   );
-}
+};
 
 export default App;
